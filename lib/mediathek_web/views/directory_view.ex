831c779cad7b98ba0b92f03fc726fe31f1689e50
Niklas Heise <n.heise@heise-media.de>
2019-01-15 19:38:23 +0100

    when serve web request remove second level subs

--- lib/mediathek_web/views/directory_view.ex
+++ lib/mediathek_web/views/directory_view.ex
@@ -6,5 +6,17 @@ defmodule MediathekWeb.DirectoryView do
 
   def render("directory.json", %{directory: directory}) do
     directory
+    |> remove_second_sub_directory_level()
+  end
+
+  defp remove_second_sub_directory_level( directory ) do
+    directory
+    |> Map.get( :subs )
+    |> Enum.map( &(%{ &1 | subs: [] }) )
+    |> update_directory_subs( directory )
+  end
+
+  defp update_directory_subs( subs, directory ) do
+    %{ directory | subs: subs }
   end
 end
--- test/mediathek_web/controllers/directory_controller_test.exs
+++ test/mediathek_web/controllers/directory_controller_test.exs
@@ -4,14 +4,44 @@ defmodule MediathekWeb.DirectoryControllerTest do
   alias Mediathek.Directory.Cache
   alias Directory.CacheItem
 
+  @sub_sub_dir %CacheItem{
+    full_path: "/fullPath/sub_dir/sub_dir",
+    cache_path: "sub_dir/sub_dir",
+    parent_cache_path: "sub_dir",
+    name: "sub_dir",
+    type: :directory,
+    size: 5,
+    subs: []
+  }
+
+  @sub_dir %CacheItem{
+    full_path: "/fullPath/sub_dir",
+    cache_path: "sub_dir",
+    parent_cache_path: "",
+    name: "sub_dir",
+    type: :directory,
+    size: 5,
+    subs: [@sub_sub_dir]
+  }
+
+  @sub_file %CacheItem{
+    full_path: "/fullPath/sub_file",
+    cache_path: "sub_file",
+    parent_cache_path: "",
+    name: "sub_file",
+    type: :regular,
+    size: 5,
+    subs: []
+  }
+
   @root_item %CacheItem{
-    full_path: "fullPath",
+    full_path: "/fullPath",
     cache_path: "",
     parent_cache_path: "",
     name: "",
     type: :directory,
     size: 5,
-    subs: []
+    subs: [@sub_dir, @sub_file]
   }
   @cache %{
     {""} => @root_item
@@ -21,8 +51,7 @@ defmodule MediathekWeb.DirectoryControllerTest do
     setup [:create_cache]
 
     test "Response with root path", %{conn: conn, cache: _cache} do
-
-      #IO.inspect(conn)
+      # IO.inspect(conn)
 
       response =
         conn
@@ -31,21 +60,41 @@ defmodule MediathekWeb.DirectoryControllerTest do
 
       expected = %{
         "cache_path" => "",
-        "full_path" => "fullPath",
         "name" => "",
         "parent_cache_path" => "",
         "size" => 5,
-        "subs" => [],
-        "type" => "directory"
+        "type" => "directory",
+        "full_path" => "/fullPath",
+        "subs" => [
+          %{
+            "cache_path" => "sub_dir",
+            "full_path" => "/fullPath/sub_dir",
+            "name" => "sub_dir",
+            "parent_cache_path" => "",
+            "size" => 5,
+            "subs" => [],
+            "type" => "directory"
+          },
+          %{
+            "cache_path" => "sub_file",
+            "full_path" => "/fullPath/sub_file",
+            "name" => "sub_file",
+            "parent_cache_path" => "",
+            "size" => 5,
+            "subs" => [],
+            "type" => "regular"
+          }
+        ]
       }
-      #IO.inspect(response)
+
+      # IO.inspect(response)
 
       assert response == expected
     end
   end
 
   defp create_cache(_) do
-    cache = start_supervised!({Cache, (fn -> @cache end)} )
+    cache = start_supervised!({Cache, fn -> @cache end})
     {:ok, cache: cache}
   end
 end
